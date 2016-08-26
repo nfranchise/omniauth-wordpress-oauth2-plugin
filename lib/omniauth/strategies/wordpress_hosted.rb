b176ff66040c4de479e64e2e42edcd9cb64ef4b9
nfranchise <9nf1@queensu.ca>
2016-08-26 14:49:32 -0400

    Update wordpress_hosted.rb

--- lib/omniauth/strategies/wordpress_hosted.rb
+++ lib/omniauth/strategies/wordpress_hosted.rb
@@ -32,9 +32,15 @@ module OmniAuth
       end
 
       def raw_info
-       access_token.get(options[:client_options][:access_url]).parsed || {}
-
+        puts access_token.token
+        @raw_info ||= access_token.get(
+          "/oauth/me",
+          :params => { 'Authorization' =>
+                       "Bearer #{access_token.token}"
+                       }
+        ).parsed
       end
+      
         def callback_url
            full_host + script_name + callback_path
         end
